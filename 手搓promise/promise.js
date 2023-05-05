13977b70128540bcf8eab5f9e8d63302d581557b
Yowo-moran <zsj2805547052@163.com>
2023-05-05 21:00:32 +0800

    内部函数代码实现

--- 手搓promise/promise.js
+++ 手搓promise/promise.js
@@ -1,11 +1,26 @@
+//声明构造函数
 function Promise(executor){
+    //添加属性
+    this.PromiseState = "penging";
+    this.PromiseResult = null;
+
+    //保存实例对象的 this 的值
+    const self = this;
+
     //resolve 函数
     function resolve(data){
-
+        //1.修改对象的状态(promiseState)
+        self.PromiseState = "fulfilled";//resolved
+        //2.设置对象结果值(promiseResult)
+        self.PromiseResult = data;
     }
+
     //reject 函数
     function reject(data){
-
+        //1.修改对象的状态(promiseState)
+        self.PromiseState = "rejected";
+        //2.设置对象结果值(promiseResult)
+        self.PromiseResult = data;
     }
 
     //同步调用“执行器函数”
