cff6047089fe6656ea54988827d7ea2c0425d775
Yowo-moran <zsj2805547052@163.com>
2023-05-05 21:13:09 +0800

    抛出异常改变状态

--- 手搓promise/promise.js
+++ 手搓promise/promise.js
@@ -22,9 +22,15 @@ function Promise(executor){
         //2.设置对象结果值(promiseResult)
         self.PromiseResult = data;
     }
+    
+    try{
+        //同步调用“执行器函数”
+        executor(resolve,reject);
+    }catch(e){
+        //修改promise对象状态为“失败”
+        reject(e);
+    }
 
-    //同步调用“执行器函数”
-    executor(resolve,reject);
 }
 
 //添加 then 方法
